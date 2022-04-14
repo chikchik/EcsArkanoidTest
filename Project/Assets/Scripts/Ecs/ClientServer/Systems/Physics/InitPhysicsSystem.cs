538a034da2d0f74150fc395644f3739be048a742
Vitaly Yeutukh <vitali.yeutukh@fabros-team.com>
2022-04-14 13:02:52 +0300

    1. Fix

--- Project/Assets/Scripts/Ecs/ClientServer/Systems/Physics/InitPhysicsSystem.cs
+++ Project/Assets/Scripts/Ecs/ClientServer/Systems/Physics/InitPhysicsSystem.cs
@@ -5,18 +5,11 @@ using Leopotam.EcsLite;
 
 namespace Game.Ecs.ClientServer.Systems.Physics
 {
-    public class InitPhysicsSystem : IEcsInitSystem, IEcsWorldChangedSystem
+    public class InitPhysicsSystem : IEcsWorldChangedSystem
     {
-        public void Init(EcsSystems systems)
-        {
-            var world = systems.GetWorld();
-            ref var physicsWorldComponent = ref world.AddUnique<PhysicsWorldComponent>();
-            physicsWorldComponent.worldReference = Box2DPhysics.CreateWorld(Config.GRAVITY);           
-        }
-
         public void WorldChanged(EcsWorld world)
         {
-            if (world.HasUnique<PhysicsWorldComponent>())
+            if (!world.HasUnique<PhysicsWorldComponent>())
             {
                 ref var physicsWorldComponent = ref world.AddUnique<PhysicsWorldComponent>();
                 physicsWorldComponent.worldReference = Box2DPhysics.CreateWorld(Config.GRAVITY);   
