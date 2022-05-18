fd136fd2ef572eebab83e1894871489cbe28b0d4
yburienkov <y.burienkov@moonmana.com>
2022-05-18 17:28:28 +0100

    minor

--- Project/Assets/Scripts/Box2dGizmosView.cs
+++ Project/Assets/Scripts/Box2dGizmosView.cs
@@ -12,9 +12,12 @@ namespace Game
     {
         [Inject] private EcsWorld _ecsWorld = null;
         private IntPtr _physicsWorldRef = IntPtr.Zero;
+
+        [SerializeField] private bool show = false;
         
         private void OnDrawGizmos()
         {
+            if (!show) return;
             if (_ecsWorld != null && !_ecsWorld.HasUnique<PhysicsWorldComponent>()) return;
             if (_ecsWorld != null && _physicsWorldRef == IntPtr.Zero)
                 _physicsWorldRef = _ecsWorld.GetUnique<PhysicsWorldComponent>().WorldReference;
