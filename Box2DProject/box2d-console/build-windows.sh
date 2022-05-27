d19215b608811f3b6b45dde42346e24979571b83
yburienkov <y.burienkov@moonmana.com>
2022-05-27 19:43:46 +0100

    add .pbd to lib folders

--- Box2DProject/box2d-console/build-windows.sh
+++ Box2DProject/box2d-console/build-windows.sh
@@ -5,7 +5,7 @@ buildWrapperAndLink()
     local LIB_PATH="./libbox2d.dll"
     local GAME_PATH="./build/src/Debug"
 
-    rm -rf ./out && rm -rf ./build && (cmake -S . -B ./build) && cmake --build ./build -j 4 && mv build/src/Debug/box2dConsole.exe ./ && (echo "$(tput setaf 2)BUILD COMPLETE, COPYING...$(tput setaf 3)")
+    rm -rf ./out && rm -rf ./build && (cmake -S . -B ./build) && cmake --build ./build -j 4 && mv build/src/Debug/box2dConsole.exe ./ && (echo "$(tput setaf 2)BUILD COMPLETE$(tput setaf 3)")
 }
 
 buildWrapperAndLink
--- Box2DProject/build-windows.sh
+++ Box2DProject/build-windows.sh
@@ -13,11 +13,12 @@ cd ./box2d-unity-wrapper
 buildWrapperAndLink()
 {
     local LIB_PATH="./build/box2dWrapper/src/Debug/libbox2d.dll"
+    local LIB_DBG_PATH="./build/box2dWrapper/src/Debug/libbox2d.pdb"
     local SERVER_PATH="../../ServerApp/"
     local GAME_PATH="../../Project/Assets/Plugins/LibBox2D/Windows"
     local TEST_PATH="../box2d-console"
 
-    ./build-windows.sh && (echo "$(tput setaf 2)BUILD COMPLETE, COPYING...$(tput setaf 3)" && cp -v $LIB_PATH $SERVER_PATH && cp -v $LIB_PATH $GAME_PATH && cp -v $LIB_PATH $TEST_PATH && echo "$(tput setaf 2)DONE!" || echo "$(tput setaf 1)ERROR!") || echo "$(tput setaf 1)BUILD ERROR!"
+    ./build-windows.sh && (echo "$(tput setaf 2)BUILD COMPLETE, COPYING...$(tput setaf 3)" && cp -v $LIB_PATH $SERVER_PATH && cp -v $LIB_PATH $GAME_PATH && cp -v $LIB_PATH $TEST_PATH && cp -v $LIB_DBG_PATH $SERVER_PATH && cp -v $LIB_DBG_PATH $GAME_PATH && cp -v $LIB_DBG_PATH $TEST_PATH && echo "$(tput setaf 2)DONE!" || echo "$(tput setaf 1)ERROR!") || echo "$(tput setaf 1)BUILD ERROR!"
 }
 
 buildWrapperAndLink
