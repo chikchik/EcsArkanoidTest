82c770b37b472b7745e11d63adb5487d2a15a82e
denism <frankinshtein85@gmail.com>
2022-07-18 19:55:54 +0500

    outdated folder

--- Box2DProject/box2d-framework/include/box2d/b2_allocator_constants.h
+++
@@ -1,8 +0,0 @@
-#ifndef B2_ALLOCATOR_CONSTANTS_H
-#define B2_ALLOCATOR_CONSTANTS_H
-
-#include "b2_types.h"
-
-static const int32 b2_chunkSize = 16 * 1024;
-
-#endif
--- Box2DProject/box2d-unity-wrapper/build-windows.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-rm -rf ./build && (cmake -S . -B ./build) && cmake --build ./build -j 4
