c25b3f7f0f2a84619c37db8436a9feaea4296bc6
denism <frankinshtein85@gmail.com>
2022-07-27 02:25:13 +0500

    wip

--- Project/Assets/Scripts/Ecs/ClientServer/Components/Input/proto/InputActionComponent.cs
+++
@@ -1,8 +0,0 @@
-﻿using Game.Fabros.Net.ClientServer.Ecs.Components;
-
-namespace Game.Ecs.ClientServer.Components.Input.proto
-{
-    public struct InputActionComponent:IInputComponent
-    {
-    }
-}
