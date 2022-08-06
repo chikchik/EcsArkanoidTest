3a2f15f0974467f37ad38f1ce6ddc92341a40583
Aleksei Golovkin <69351628+spacekorben-major@users.noreply.github.com>
2022-08-06 20:23:42 +0400

    added capacity check

--- Project/Assets/Scripts/ClientServer/Services/MyInventoryService.cs
+++ Project/Assets/Scripts/ClientServer/Services/MyInventoryService.cs
@@ -1,4 +1,7 @@
-﻿using XFlow.EcsLite;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using XFlow.EcsLite;
+using XFlow.Modules.Inventory.Client.Interfaces;
 using XFlow.Modules.Inventory.ClientServer;
 using XFlow.Modules.Inventory.ClientServer.Components;
 using XFlow.Utils;
@@ -7,6 +10,13 @@ namespace Game.ClientServer.Services
 {
     public class MyInventoryService : InventoryService
     {
+        private readonly IInventoryStaticData staticData;
+
+        public MyInventoryService(IInventoryStaticData staticData)
+        {
+            this.staticData = staticData;
+        }
+        
         public override bool IsItemStackable(EcsWorld world, int itemEntity)
         {
             var poolStackable = world.GetPool<StackableComponent>();
@@ -32,6 +42,37 @@ namespace Game.ClientServer.Services
             if (GetEntityAmount(world, itemEntity) == 0)
             {
                 itemEntity.EntityAdd<EntityRemoveEventComponent>(world);
+            }
+        }
+
+        public override bool IsEnoughSpaceForItems(EcsWorld world, int inventoryEntity, string itemId, int amount,
+            bool isStackable)
+        {
+            int slotCapacity = isStackable 
+                ? inventoryEntity.EntityGet<InventoryComponent>(world).SlotCapacity
+                : 1;
+
+            string categoryId = staticData.GetInventoryItemInfo(itemId).CategoryId;
+
+            HasItems(world, inventoryEntity, out List<int> filledSlots, Predicate);
+            HasItems(world, inventoryEntity, itemId, out List<int> slotsWithItem, Predicate);
+
+            int filledSlotsCount = filledSlots.Count;
+            int filledSlotsWithItemCount = slotsWithItem.Count;
+            int filledSlotsByOtherItemsCount = filledSlotsCount - filledSlotsWithItemCount;
+            
+            int amountItems = GetAmount(world, inventoryEntity, itemId);
+            int possibleAmount = amountItems + amount;
+            int possibleSlots = Mathf.CeilToInt((float)possibleAmount / (float)slotCapacity);
+
+            return possibleSlots + filledSlotsByOtherItemsCount <= staticData.MaxSlotsInCategory;
+
+            bool Predicate(int slotEntity)
+            {
+                string slotItemId = slotEntity.EntityGet<ItemIdComponent>(world).ItemId;
+                string slotCategoryId = staticData.GetInventoryItemInfo(slotItemId).CategoryId;
+
+                return slotCategoryId == categoryId;
             }
         }
     }
