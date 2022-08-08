3264e38e17349d3dc0fe60469c488df5969fa259
denism <frankinshtein85@gmail.com>
2022-08-08 16:52:29 +0500

    minor

--- ServerApp/tools/upload.py
+++ ServerApp/tools/upload.py
@@ -1,8 +1,26 @@
 import requests
-import io
+import zipfile
+import os
+
 SECRET = "iun432qezZqw"
-#HOST = "http://localhost:5000"
-HOST = "https://c1.ecs.fbpub.net"
+CONTAINER_FILES = "../ServerApp/bin/Debug/netcoreapp3.1"
+HOST = "http://localhost:5000"
+#HOST = "https://c1.ecs.fbpub.net"
+
+
+
+zip = zipfile.ZipFile("container.zip", 'w', zipfile.ZIP_DEFLATED)
+
+def add_dir(path, zf):
+    for file in os.listdir(path):
+        src = f"{path}/{file}"
+        print(f"added to zip {src}")
+        zf.write(src, file)
+
+with zipfile.ZipFile("container.zip", 'w', zipfile.ZIP_DEFLATED) as zf:
+    add_dir(CONTAINER_FILES, zf)
+
+
 args = "ROOM=5000"
 with open("container.zip", 'rb') as file:
     url = f"{HOST}/{SECRET}/upload?{args}"
