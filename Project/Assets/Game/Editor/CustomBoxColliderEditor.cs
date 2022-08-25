22d1d027f65384cafc4e42b75819d5eab40a26d0
anton.gaag <anton.gaag@ilogos.biz>
2022-08-25 19:13:21 +0700

    [FE-144] BoxCollider rotation fix

--- Project/Assets/Game/Editor/CustomBoxColliderEditor.cs
+++ Project/Assets/Game/Editor/CustomBoxColliderEditor.cs
@@ -19,39 +19,38 @@ public class CustomBoxColliderEditor : Editor
         if (!targetCollider.enabled) return;
 
         Handles.color = _editMode ? Color.green : Color.green * 0.7f;
-        var p = targetCollider.transform.position;
+        var pos = targetCollider.transform.position;
+        var rot = targetCollider.transform.rotation;
         var halfsize = targetCollider.size * 0.5f;
         var lineVertices = new[]
         {
-            new Vector3(p.x + halfsize.x, p.y, p.z + halfsize.y),
-            new Vector3(p.x - halfsize.x, p.y, p.z + halfsize.y),
-            new Vector3(p.x - halfsize.x, p.y, p.z - halfsize.y),
-            new Vector3(p.x + halfsize.x, p.y, p.z - halfsize.y),
-            new Vector3(p.x + halfsize.x, p.y, p.z + halfsize.y),
+            rot * new Vector3(halfsize.x, 0f, halfsize.y) + pos,
+            rot * new Vector3(-halfsize.x, 0f, halfsize.y) + pos,
+            rot * new Vector3(-halfsize.x, 0f, -halfsize.y) + pos,
+            rot * new Vector3(halfsize.x, 0f, -halfsize.y) + pos,
+            rot * new Vector3(halfsize.x, 0f, halfsize.y) + pos,
         };
         Handles.DrawPolyLine(lineVertices);
-        //Handles.DrawWireDisc(targetCollider.transform.position, Vector3.up, targetCollider.radius);
         if (!_editMode) return;
 
         var handlers = new[]
         {
-            (new Vector3(p.x, p.y, p.z + halfsize.y), Vector3.forward),
-            (new Vector3(p.x, p.y, p.z - halfsize.y), Vector3.forward),
-            (new Vector3(p.x + halfsize.x, p.y, p.z), Vector3.right),
-            (new Vector3(p.x - halfsize.x, p.y, p.z), Vector3.right),
+            (rot * new Vector3(0f, 0f, halfsize.y) + pos, Vector3.forward),
+            (rot * new Vector3(0f, 0f, -halfsize.y) + pos, Vector3.forward),
+            (rot * new Vector3(halfsize.x, 0f, 0f) + pos, Vector3.right),
+            (rot * new Vector3(-halfsize.x, 0f, 0f) + pos, Vector3.right),
         };
 
         for (int i = 0; i < handlers.Length; i++)
         {
             EditorGUI.BeginChangeCheck();
             _handlerSize = HandleUtility.GetHandleSize(handlers[i].Item1) * 0.05f;
-            handlers[i].Item1 = Handles.Slider(handlers[i].Item1, handlers[i].Item2, _handlerSize, Handles.DotHandleCap, 0f);
+            handlers[i].Item1 = Handles.Slider(handlers[i].Item1, rot * handlers[i].Item2, _handlerSize, Handles.DotHandleCap, 0f);
             if (EditorGUI.EndChangeCheck())
             {
                 Undo.RecordObject(targetCollider, "Change collider size");
-                var diff = p - handlers[i].Item1;
-                if (handlers[i].Item2 == Vector3.right) targetCollider.size = new Vector2(Mathf.Abs(diff.x) * 2, targetCollider.size.y);
-                else targetCollider.size = new Vector2(targetCollider.size.x, Mathf.Abs(diff.z) * 2);
+                var diff = pos - handlers[i].Item1;
+                targetCollider.size = handlers[i].Item2 == Vector3.right ? new Vector2(diff.magnitude * 2, targetCollider.size.y) : new Vector2(targetCollider.size.x, diff.magnitude * 2);
                 break;
             }
         }
