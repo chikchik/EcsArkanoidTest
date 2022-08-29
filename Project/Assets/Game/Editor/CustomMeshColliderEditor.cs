cb02be83c18f5df50e88d533e366190c55686a0f
anton.gaag <anton.gaag@ilogos.biz>
2022-08-29 15:02:45 +0700

    [FE-144] Polygon collider full

--- Project/Assets/Game/Editor/CustomMeshColliderEditor.cs
+++ Project/Assets/Game/Editor/CustomMeshColliderEditor.cs
@@ -8,15 +8,18 @@ using UnityEngine;
 public class CustomMeshColliderEditor : Editor
 {
     private bool _editMode;
+    private bool _removeMode;
     private PolygonCollider2D _target;
     private Vector3 _mouseProjection;
     private int _activePathIndex;
     private int _activeVerticeIndex;
     private int _activeEdgeVerticeIndex;
-    private Vector3 _dissectionPoint;
+    private Vector2 _dissectionPoint;
+    private Vector3 _dissectionHandler;
     private Quaternion _rotation;
 
     private const float _distanceThreshold = 1f;
+    private const float _verticeSelectTreshold = 0.15f;
 
     public override void OnInspectorGUI()
     {
@@ -26,12 +29,15 @@ public class CustomMeshColliderEditor : Editor
 
     public void OnSceneGUI()
     {
-        Debug.Log(Event.current.type);
         _target = target as PolygonCollider2D;
         DrawColliderShapes();
 
         if (!_editMode) return;
-        if (Event.current.type == EventType.MouseMove) DetectActiveSelection();
+        if (Event.current.type == EventType.Layout) HandleUtility.AddDefaultControl(0);
+        if (Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.LeftControl) _removeMode = true;
+        if (Event.current.type == EventType.KeyUp && Event.current.keyCode == KeyCode.LeftControl) _removeMode = false;
+        DetectActiveSelection();
+        SceneView.RepaintAll();
         DrawHandlers();
     }
 
@@ -58,9 +64,14 @@ public class CustomMeshColliderEditor : Editor
         var activeVert = path[_activeVerticeIndex];
         if (_activeEdgeVerticeIndex == -1)
         {
+            if (Event.current.type == EventType.MouseDown && _removeMode)
+            {
+                RemoveVertice();
+                return;
+            }
             EditorGUI.BeginChangeCheck();
             var handlePos = _rotation * new Vector3(path[_activeVerticeIndex].x, 0f, path[_activeVerticeIndex].y) + _target.transform.position;
-            handlePos = Handles.FreeMoveHandle(handlePos, _rotation, 0.2f, Vector3.zero, Handles.DotHandleCap);
+            handlePos = Handles.FreeMoveHandle(handlePos, _rotation, HandleUtility.GetHandleSize(handlePos) * 0.05f, Vector3.zero, Handles.DotHandleCap);
             if (EditorGUI.EndChangeCheck())
             {
                 Undo.RecordObject(_target, "Change collider shape");
@@ -73,8 +84,61 @@ public class CustomMeshColliderEditor : Editor
 
         var activeEdgeVert = path[_activeEdgeVerticeIndex];
         Handles.DrawLine(_rotation * new Vector3(activeVert.x, 0f, activeVert.y) + _target.transform.position,
-            _rotation * new Vector3(activeEdgeVert.x, 0f, activeEdgeVert.y) + _target.transform.position, 10f);
-        Handles.DotHandleCap(0, _dissectionPoint, Quaternion.identity, 0.2f, EventType.Repaint);
+            _rotation * new Vector3(activeEdgeVert.x, 0f, activeEdgeVert.y) + _target.transform.position, 3f);
+        Handles.DotHandleCap(0, _dissectionHandler, Quaternion.identity, HandleUtility.GetHandleSize(_dissectionHandler) * 0.05f, EventType.Repaint);
+
+        if (Event.current.type == EventType.MouseDown && !_removeMode)
+        {
+            DissectEdge();
+            Event.current = null;
+        }
+    }
+
+    private void DissectEdge()
+    {
+        var path = _target.GetPath(_activePathIndex);
+        var newPath = new Vector2[path.Length + 1];
+        var shift = 0;
+        var ind = Mathf.Min(_activeEdgeVerticeIndex, _activeVerticeIndex);
+        var newPos = ind + 1;
+        if ((_activeVerticeIndex == 0 && _activeEdgeVerticeIndex == path.Length - 1) || (_activeVerticeIndex == path.Length - 1 && _activeEdgeVerticeIndex == 0))
+        {
+            newPos = -1; //special case, add vertice at end
+        }
+
+        for (var i = 0; i < path.Length; i++)
+        {
+            if (i == newPos)
+            {
+                newPath[i] = _dissectionPoint;
+                shift = 1;
+            }
+            newPath[i + shift] = path[i];
+        }
+
+        if (newPos == -1) newPath[newPath.Length - 1] = _dissectionPoint;
+
+        Undo.RecordObject(_target, "Add collider vertice");
+        _target.SetPath(_activePathIndex, newPath);
+    }
+
+    private void RemoveVertice()
+    {
+        var shift = 0;
+        var path = _target.GetPath(_activePathIndex);
+        var newPath = new Vector2[path.Length - 1];
+        for (var i = 0; i < path.Length; i++)
+        {
+            if (i == _activeVerticeIndex)
+            {
+                shift = -1;
+                continue;
+            }
+
+            newPath[i + shift] = path[i];
+        }
+        Undo.RecordObject(_target, "Delete collider vertice");
+        _target.SetPath(_activePathIndex, newPath);
     }
 
     private void DetectActiveSelection()
@@ -89,35 +153,31 @@ public class CustomMeshColliderEditor : Editor
 
         _mouseProjection = Quaternion.Inverse(_rotation) * (ray.GetPoint(distance) - _target.transform.position);
         var mouse2d = new Vector2(_mouseProjection.x, _mouseProjection.z);
-        //var mouse3d = _rotation * new Vector3(mouse2d.x, 0f, mouse2d.y);
-        //Handles.DrawWireDisc(_target.transform.position + mouse3d, Vector3.up, 0.5f);
         var nearest = GetNearestVertice(mouse2d, out distance);
         _activePathIndex = nearest.x;
         _activeVerticeIndex = nearest.y;
 
         var path = _target.GetPath(_activePathIndex);
-        Vector2 cross;
 
         //check edge to prev vert
         var i = _activeVerticeIndex == 0 ? path.Length - 1 : _activeVerticeIndex - 1;
-        if (CheckAngle(path[_activeVerticeIndex], path[i], mouse2d, out cross))
+        if (CheckAngle(path[_activeVerticeIndex], path[i], mouse2d, out _dissectionPoint))
         {
             _activeEdgeVerticeIndex = i;
-            _dissectionPoint = _rotation * new Vector3(cross.x, 0f, cross.y) + _target.transform.position;
+            _dissectionHandler = _rotation * new Vector3(_dissectionPoint.x, 0f, _dissectionPoint.y) + _target.transform.position;
             return;
         }
 
         //check edge to next vert
         i = _activeVerticeIndex == path.Length - 1 ? 0 : _activeVerticeIndex + 1;
-        if (CheckAngle(path[_activeVerticeIndex], path[i], mouse2d, out cross))
+        if (CheckAngle(path[_activeVerticeIndex], path[i], mouse2d, out _dissectionPoint))
         {
             _activeEdgeVerticeIndex = i;
-            _dissectionPoint = _rotation * new Vector3(cross.x, 0f, cross.y) + _target.transform.position;
+            _dissectionHandler = _rotation * new Vector3(_dissectionPoint.x, 0f, _dissectionPoint.y) + _target.transform.position;
             return;
         }
 
         _activeEdgeVerticeIndex = -1;
-        return;
 
     }
 
@@ -126,7 +186,7 @@ public class CustomMeshColliderEditor : Editor
         var edge = edgevert - vert;
         var mouseDir = mouse - vert;
         var dot = Vector2.Dot(edge.normalized, mouseDir);
-        if (dot < 0 || dot > edge.magnitude)
+        if (dot < _verticeSelectTreshold || dot > edge.magnitude)
         {
             cross = Vector2.zero;
             return false;
