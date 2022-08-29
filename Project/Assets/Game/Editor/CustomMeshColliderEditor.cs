48a4836dc0b58610effe062f3c80d683eeb4a4f0
anton.gaag <anton.gaag@ilogos.biz>
2022-08-29 15:46:13 +0700

    [FE-144] Code cleanup

--- Project/Assets/Game/Editor/CustomMeshColliderEditor.cs
+++ Project/Assets/Game/Editor/CustomMeshColliderEditor.cs
@@ -10,12 +10,10 @@ public class CustomMeshColliderEditor : Editor
     private bool _editMode;
     private bool _removeMode;
     private PolygonCollider2D _target;
-    private Vector3 _mouseProjection;
     private int _activePathIndex;
     private int _activeVerticeIndex;
     private int _activeEdgeVerticeIndex;
     private Vector2 _dissectionPoint;
-    private Vector3 _dissectionHandler;
     private Quaternion _rotation;
 
     private const float _distanceThreshold = 1f;
@@ -30,6 +28,10 @@ public class CustomMeshColliderEditor : Editor
     public void OnSceneGUI()
     {
         _target = target as PolygonCollider2D;
+        if (_target == null) return;
+
+        var rotEuler = _target.transform.rotation.eulerAngles;
+        _rotation = Quaternion.Euler(new Vector3(0f, rotEuler.y, 0f));
         DrawColliderShapes();
 
         if (!_editMode) return;
@@ -45,8 +47,6 @@ public class CustomMeshColliderEditor : Editor
     {
         Handles.color = _editMode ? Color.green : Color.green * 0.7f;
         var pos = _target.transform.position;
-        var rotEuler = _target.transform.rotation.eulerAngles;
-        _rotation = Quaternion.Euler(new Vector3(0f, rotEuler.y, 0f));
         for (var i = 0; i < _target.pathCount; i++)
         {
             var lineVerts = _target.GetPath(i).Select(vert => _rotation * new Vector3(vert.x, 0f, vert.y) + pos).ToList();
@@ -70,22 +70,21 @@ public class CustomMeshColliderEditor : Editor
                 return;
             }
             EditorGUI.BeginChangeCheck();
-            var handlePos = _rotation * new Vector3(path[_activeVerticeIndex].x, 0f, path[_activeVerticeIndex].y) + _target.transform.position;
+            var handlePos = FromColliderSpace(activeVert);
             handlePos = Handles.FreeMoveHandle(handlePos, _rotation, HandleUtility.GetHandleSize(handlePos) * 0.05f, Vector3.zero, Handles.DotHandleCap);
             if (EditorGUI.EndChangeCheck())
             {
                 Undo.RecordObject(_target, "Change collider shape");
-                var loc = Quaternion.Inverse(_rotation) * (handlePos - _target.transform.position);
-                path[_activeVerticeIndex] = new Vector2(loc.x, loc.z);
+                path[_activeVerticeIndex] = ToColliderSpace(handlePos);
                 _target.SetPath(_activePathIndex, path);
             }
             return;
         }
 
         var activeEdgeVert = path[_activeEdgeVerticeIndex];
-        Handles.DrawLine(_rotation * new Vector3(activeVert.x, 0f, activeVert.y) + _target.transform.position,
-            _rotation * new Vector3(activeEdgeVert.x, 0f, activeEdgeVert.y) + _target.transform.position, 3f);
-        Handles.DotHandleCap(0, _dissectionHandler, Quaternion.identity, HandleUtility.GetHandleSize(_dissectionHandler) * 0.05f, EventType.Repaint);
+        Handles.DrawLine(FromColliderSpace(activeVert), FromColliderSpace(activeEdgeVert), 3f);
+        var dissection = FromColliderSpace(_dissectionPoint);
+        Handles.DotHandleCap(0, dissection, Quaternion.identity, HandleUtility.GetHandleSize(dissection) * 0.05f, EventType.Repaint);
 
         if (Event.current.type == EventType.MouseDown && !_removeMode)
         {
@@ -151,8 +150,7 @@ public class CustomMeshColliderEditor : Editor
             return;
         }
 
-        _mouseProjection = Quaternion.Inverse(_rotation) * (ray.GetPoint(distance) - _target.transform.position);
-        var mouse2d = new Vector2(_mouseProjection.x, _mouseProjection.z);
+        var mouse2d = ToColliderSpace(ray.GetPoint(distance));
         var nearest = GetNearestVertice(mouse2d, out distance);
         _activePathIndex = nearest.x;
         _activeVerticeIndex = nearest.y;
@@ -164,7 +162,6 @@ public class CustomMeshColliderEditor : Editor
         if (CheckAngle(path[_activeVerticeIndex], path[i], mouse2d, out _dissectionPoint))
         {
             _activeEdgeVerticeIndex = i;
-            _dissectionHandler = _rotation * new Vector3(_dissectionPoint.x, 0f, _dissectionPoint.y) + _target.transform.position;
             return;
         }
 
@@ -173,7 +170,6 @@ public class CustomMeshColliderEditor : Editor
         if (CheckAngle(path[_activeVerticeIndex], path[i], mouse2d, out _dissectionPoint))
         {
             _activeEdgeVerticeIndex = i;
-            _dissectionHandler = _rotation * new Vector3(_dissectionPoint.x, 0f, _dissectionPoint.y) + _target.transform.position;
             return;
         }
 
@@ -215,5 +211,17 @@ public class CustomMeshColliderEditor : Editor
         }
 
         return nearest;
+    }
+
+    private Vector2 ToColliderSpace(Vector3 vector)
+    {
+        var loc = Quaternion.Inverse(_rotation) * (vector - _target.transform.position);
+        return new Vector2(loc.x, loc.z);
+    }
+
+    private Vector3 FromColliderSpace(Vector2 vector)
+    {
+        var res = _rotation * new Vector3(vector.x, 0f, vector.y) + _target.transform.position;
+        return res;
     }
 }
