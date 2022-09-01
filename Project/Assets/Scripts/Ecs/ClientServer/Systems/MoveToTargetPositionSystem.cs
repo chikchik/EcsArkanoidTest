dd2bab27452ae03bd6a2cbc34d4bd96ece40b914
anton.gaag <anton.gaag@ilogos.biz>
2022-09-01 16:29:25 +0700

    [FE-177] Units with dynamic rigid bodies

--- Project/Assets/Scripts/Ecs/ClientServer/Systems/MoveToTargetPositionSystem.cs
+++ Project/Assets/Scripts/Ecs/ClientServer/Systems/MoveToTargetPositionSystem.cs
@@ -4,6 +4,7 @@ using Game.Ecs.ClientServer.Components;
 using UnityEngine;
 using XFlow.Ecs.ClientServer.Components;
 using XFlow.EcsLite;
+using XFlow.Modules.Box2D.ClientServer.Components;
 using XFlow.Modules.Tick.Other;
 using XFlow.Utils;
 
@@ -11,14 +12,17 @@ namespace Game.Ecs.ClientServer.Systems
 {
     public class MoveToTargetPositionSystem : IEcsRunSystem, IEcsInitSystem
     {
-        EcsPool<TargetPositionComponent> poolTargetPosition;
-        EcsPool<PositionComponent> poolPosition;
-        EcsPool<MovingComponent> poolMoving;
-        EcsPool<CantMoveComponent> poolCantMove;
-        EcsPool<LookDirectionComponent> poolLookDirection;
-        EcsPool<AverageSpeedComponent> poolAverageSpeed;
-        EcsWorld world;
-        
+        private EcsPool<TargetPositionComponent> poolTargetPosition;
+        private EcsPool<PositionComponent> poolPosition;
+        private EcsPool<MovingComponent> poolMoving;
+        private EcsPool<CantMoveComponent> poolCantMove;
+        private EcsPool<LookDirectionComponent> poolLookDirection;
+        private EcsPool<AverageSpeedComponent> poolAverageSpeed;
+        private EcsPool<Box2DRigidbodyComponent> poolRigidBody;
+        private EcsWorld world;
+
+        private const float _distanceTolerance = 0.1f;
+
         public void Init(EcsSystems systems)
         {
             world = systems.GetWorld();
@@ -28,15 +32,9 @@ namespace Game.Ecs.ClientServer.Systems
             poolMoving = world.GetPool<MovingComponent>();
             poolCantMove = world.GetPool<CantMoveComponent>();
             poolAverageSpeed = world.GetPool<AverageSpeedComponent>();
+            poolRigidBody = world.GetPool<Box2DRigidbodyComponent>();
         }
 
-        private void ReachedTarget(int entity, Vector3 targetPosition)
-        {
-            poolPosition.GetRef(entity).value = targetPosition;
-            poolTargetPosition.Del(entity);
-            poolMoving.Del(entity);
-        }
-        
         public void Run(EcsSystems systems)
         {
             var filter = world
@@ -49,33 +47,30 @@ namespace Game.Ecs.ClientServer.Systems
             foreach (var entity in filter)
             {
                 if (poolCantMove.Has(entity))
+                {
+                    poolRigidBody.GetRef(entity).LinearVelocity = Vector2.zero;
                     continue;
-                
+                }
+
                 var targetPositionComponent = poolTargetPosition.Get(entity);
                 var positionComponent = poolPosition.Get(entity);
 
-                var directionToTarget = (targetPositionComponent.Value - positionComponent.value).WithY(0);
+                var directionToTarget = (targetPositionComponent.Value - positionComponent.value);
+                var direction2D = new Vector2(directionToTarget.x, directionToTarget.z);
 
                 //too close to target
-                if (directionToTarget.magnitude < 0.00001f)
+                if (direction2D.magnitude < _distanceTolerance)
                 {
-                    ReachedTarget(entity, targetPositionComponent.Value);
+                    poolRigidBody.GetRef(entity).LinearVelocity = Vector2.zero;
+                    poolTargetPosition.Del(entity);
+                    poolMoving.Del(entity);
                     continue;
                 }
-                
-                poolMoving.Replace(entity, new MovingComponent());
-                
+
                 var speed = poolAverageSpeed.GetNullable(entity)?.Value ?? 1.0f;
-                var step = directionToTarget.normalized * deltaTime * speed;
-                
-                if (step.magnitude < directionToTarget.magnitude)
-                {
-                    poolPosition.GetRef(entity).value += step;
-                    poolLookDirection.GetOrCreateRef(entity).value = step.normalized;
-                    continue;
-                }
-                
-                ReachedTarget(entity, targetPositionComponent.Value);
+                poolRigidBody.GetRef(entity).LinearVelocity = direction2D.normalized * speed;
+                poolLookDirection.GetOrCreateRef(entity).value = directionToTarget.normalized;
+                poolMoving.Replace(entity, new MovingComponent());
             }
         }
 
--- Project/Assets/Scripts/Ecs/ClientServer/Systems/UnitMoveSystem.cs
+++ Project/Assets/Scripts/Ecs/ClientServer/Systems/UnitMoveSystem.cs
@@ -8,38 +8,63 @@ using XFlow.Modules.Box2D.ClientServer.Components;
 
 namespace Game.Ecs.ClientServer.Systems
 {
-    public class UnitMoveSystem : IEcsRunSystem
+    public class UnitMoveSystem : IEcsRunSystem, IEcsInitSystem
     {
+        private EcsPool<Box2DRigidbodyComponent> _rbPool;
+        private EcsPool<MovingComponent> _movingPool;
+        private EcsPool<MoveDirectionComponent> _moveDirectionPool;
+        private EcsPool<AverageSpeedComponent> _speedPool;
+
+        private EcsFilter _moveFilter;
+        private EcsFilter _stopFilter;
+
         public void Run(EcsSystems systems)
         {
-            return;
-            var world = systems.GetWorld();
-            var filter = world
-                .Filter<MoveDirectionComponent>()
-                .Inc<AverageSpeedComponent>()
-                .Inc<UnitComponent>()
-                .Inc<Box2DBodyComponent>()
-                .End();
-            var poolMoveDirection = world.GetPool<MoveDirectionComponent>();
-            var poolSpeed = world.GetPool<AverageSpeedComponent>();
-            var poolBodyReference = world.GetPool<Box2DBodyComponent>();
-
-            foreach (var entity in filter)
+            foreach (var entity in _moveFilter)
             {
-                SetLinearVelToBody(entity, poolBodyReference, poolMoveDirection, poolSpeed);
+                SetLinearVelToBody(entity);
+                _movingPool.Replace(entity, new MovingComponent());
+            }
+
+            foreach (var entity in _stopFilter)
+            {
+                ref var rbComponent = ref _rbPool.GetRef(entity);
+                rbComponent.LinearVelocity = Vector2.zero;
             }
         }
 
-        private void SetLinearVelToBody(int entity, EcsPool<Box2DBodyComponent> poolBodyReference,
-            EcsPool<MoveDirectionComponent> poolMoveDirection, EcsPool<AverageSpeedComponent> poolSpeed)
+        private void SetLinearVelToBody(int entity)
         {
-            var bodyReference = poolBodyReference.Get(entity).BodyReference;
-            var moveDirectionComponent = poolMoveDirection.Get(entity);
-            var speedComponent = poolSpeed.Get(entity);
+            var moveDirectionComponent = _moveDirectionPool.Get(entity);
+            var speedComponent = _speedPool.Get(entity);
+            ref var rbComponent = ref _rbPool.GetRef(entity);
+            rbComponent.LinearVelocity =
+                new Vector2(moveDirectionComponent.value.x, moveDirectionComponent.value.z).normalized *
+                speedComponent.Value;
+        }
 
-            Box2DApi.SetLinearVelocity(bodyReference,
-                new Vector2(moveDirectionComponent.value.x, moveDirectionComponent.value.z) *
-                speedComponent.Value);
+        public void Init(EcsSystems systems)
+        {
+            var world = systems.GetWorld();
+            _rbPool = world.GetPool<Box2DRigidbodyComponent>();
+            _movingPool = world.GetPool<MovingComponent>();
+            _moveDirectionPool = world.GetPool<MoveDirectionComponent>();
+            _speedPool = world.GetPool<AverageSpeedComponent>();
+
+            _moveFilter = world
+                .Filter<MoveDirectionComponent>()
+                .Inc<AverageSpeedComponent>()
+                .Inc<UnitComponent>()
+                .Inc<Box2DRigidbodyComponent>()
+                .End();
+
+            _stopFilter = world
+                .Filter<UnitComponent>()
+                .Exc<MoveDirectionComponent>()
+                .Exc<TargetPositionComponent>()
+                .Inc<Box2DRigidbodyComponent>()
+                .End();
+
         }
     }
 }
