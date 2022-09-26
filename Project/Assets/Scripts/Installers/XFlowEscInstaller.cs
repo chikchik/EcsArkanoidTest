64bb9fac876d4aa146308802d25f5b8ed63a3e89
adiomkin <aliaksandr.dziomkin@fabros-team.comaliaksandr.dziomkin@fabros-team.comaliaksandr.dziomkin@fabros-team.comaliaksandr.dziomkin@fabros-team.comaliaksandr.dziomkin@fabros-team.comaliaksandr.dziomkin@fabros-team.comaliaksandr.dziomkin@fabros-team@com>
2022-09-26 15:31:21 +0300

    EBS-104 sync with dev and installer split
    # Conflicts:
    #	Project/Assets/Scripts/Installers/GameSceneInstallerAlt.cs
    #	Project/Assets/Scripts/Installers/XFlowEscInstaller.cs

---
+++ Project/Assets/Scripts/Installers/XFlowEscInstaller.cs
@@ -0,0 +1,83 @@
+using System.Net;
+using System.Threading;
+using Game.ClientServer;
+using Game.ClientServer.Services;
+using UnityEngine;
+using XFlow.Ecs.ClientServer;
+using XFlow.Ecs.ClientServer.WorldDiff;
+using XFlow.EcsLite;
+using XFlow.Net.Client;
+using XFlow.Net.ClientServer;
+using XFlow.P2P;
+using Zenject;
+
+namespace Game.Installers
+{
+    public class XFlowEscInstaller : MonoInstaller
+    {
+        [SerializeField] 
+        private GameSettings settings;
+
+        private CancellationTokenSource _cancellationTokenSource;
+
+        public sealed override void InstallBindings()
+        {
+            Container.Bind<EcsWorld>().FromInstance(new EcsWorld("main")).AsCached();
+            Container.Bind<EcsWorld>().WithId(EcsWorlds.Input).FromInstance(new EcsWorld(EcsWorlds.Input)).AsCached();
+            Container.Bind<EcsWorld>().WithId(EcsWorlds.Event).FromInstance(new EcsWorld(EcsWorlds.Event)).AsCached();
+            Container.Bind<EcsWorld>().WithId(EcsWorlds.Dead).FromInstance(new EcsWorld(EcsWorlds.Dead)).AsCached();
+            
+            Container.BindInterfacesAndSelfTo<PlayerControlService>().AsSingle();
+            Container.Bind<ClientServerServices>().AsSingle();
+
+            _cancellationTokenSource = new CancellationTokenSource();
+            Container.Bind<CancellationToken>().FromInstance(_cancellationTokenSource.Token).AsCached();
+
+            var udpHost = settings.UdpHosts[settings.SelectedUdpHostIndex];
+            var ipEndPoint = new IPEndPoint(IPAddress.Parse(udpHost.Address), udpHost.Port);
+            Container.Bind<IPEndPoint>().WithId("udp").FromInstance(ipEndPoint).AsCached();
+
+            if (settings.OverrideDefaultServerRoom)
+            {
+                var url = $"{P2P.DEV_SERVER_WS}/{settings.OverrideRoom}";
+                Container.Bind<string>().WithId("serverUrl").FromInstance(url).AsCached();
+            }
+            else
+            {
+                Container.Bind<string>().WithId("serverUrl").FromInstance(Config.URL).AsCached();    
+            }
+            
+            Container.Bind<string>().WithId("tmpHashesPath").FromInstance(Config.TMP_HASHES_PATH).AsCached();
+
+            Container.Bind<NetClient>().AsSingle();
+            
+            var collection = new ComponentsCollection();
+            ComponentsCollectionUtils.AddComponents(collection);
+            Container.Bind<ComponentsCollection>().FromInstance(collection).AsSingle();
+            
+            Container.Bind<GameSettings>().FromInstance(settings).AsSingle();
+
+            if (settings.MultiPlayer)
+            {
+                Container.Bind<NetClient>().AsSingle();
+
+                Container.Bind<IInputService>().To<ClientInputService>()
+                    .AsSingle().WhenInjectedInto<PlayerControlService>();
+            }
+            else
+            {
+                // Container.Bind<SingleGame>().AsSingle();
+            }
+            
+            DoInstallBindings(settings);
+        }
+        protected virtual void DoInstallBindings(GameSettings gameSettings) {}
+        
+        private void OnApplicationQuit()
+        {
+            Debug.Log("OnApplicationQuit");
+            _cancellationTokenSource.Cancel();
+        }
+        
+    }
+}
