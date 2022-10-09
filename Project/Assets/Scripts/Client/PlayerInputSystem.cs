00d11662e456bf45b1c4afc43d47b1b9993895f9
dm <frankinshtein85@gmail.com>
2022-10-09 20:14:51 +0300

    ref

--- Project/Assets/Scripts/Client/PlayerInputSystem.cs
+++ Project/Assets/Scripts/Client/PlayerInputSystem.cs
@@ -12,52 +12,23 @@ namespace Game.Client
         [Inject]private Joystick joystick;
         [Inject]private PlayerControlService controlService;
 
+        void MoveDir(float hor, float ver)
+        {
+            var forward = -Vector3.Cross(Vector3.up,camera.transform.right) ;
+            forward.y = 0;
+            forward.Normalize();
+
+            var right = camera.transform.right;
+            right.y = 0;
+            right.Normalize();
+
+            var dir = forward * ver + right * hor;
+                
+            controlService.MoveToDirection(dir);
+        }
+        
         public void Run(EcsSystems systems)
         {
-            CheckInput(camera, joystick, controlService);
-        }
-
-        public void Init(EcsSystems systems)
-        {
-        }
-        
-        
-        public static bool IsPointerOverUIObject()
-        {
-            PointerEventData pointer = new PointerEventData(EventSystem.current);
-            pointer.position = Input.mousePosition;
- 
-            List<RaycastResult> raycastResults = new List<RaycastResult>();
-            EventSystem.current.RaycastAll(pointer, raycastResults);
- 
-            if (raycastResults.Count > 0)
-            {
-                if (raycastResults[0].gameObject.GetComponentInParent<Canvas>() != null)
-                    return true;
-
-            }
-
-            return false;
-        }
-        
-        public static void CheckInput(Camera camera, Joystick joystick, PlayerControlService controlService)
-        {
-            void MoveDir(float hor, float ver)
-            {
-                var forward = -Vector3.Cross(Vector3.up,camera.transform.right) ;
-                forward.y = 0;
-                forward.Normalize();
-
-                var right = camera.transform.right;
-                right.y = 0;
-                right.Normalize();
-
-                var dir = forward * ver + right * hor;
-                
-                controlService.MoveToDirection(dir);
-            }
-           
-            
             if (Input.GetMouseButtonDown(0) && !IsPointerOverUIObject())
             {
                 //EventSystem.current.
@@ -86,6 +57,29 @@ namespace Game.Client
             {
                 controlService.StopMoveToDirection();
             }
+        }
+
+        public void Init(EcsSystems systems)
+        {
+        }
+        
+        
+        public static bool IsPointerOverUIObject()
+        {
+            PointerEventData pointer = new PointerEventData(EventSystem.current);
+            pointer.position = Input.mousePosition;
+ 
+            List<RaycastResult> raycastResults = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointer, raycastResults);
+ 
+            if (raycastResults.Count > 0)
+            {
+                if (raycastResults[0].gameObject.GetComponentInParent<Canvas>() != null)
+                    return true;
+
+            }
+
+            return false;
         }
     }
 }
