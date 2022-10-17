ebeda4469ab877c289c2661d0dd38fdf7bdf818a
pavel-fedorchuk <pavel.fedorchuk@ilogos.biz>
2022-10-17 11:46:46 +0300

    fix frameworkd version update submodules

--- ServerApp/tools/ContainerBuilder/Source/AppArguments/RunOptions.cs
+++ ServerApp/tools/ContainerBuilder/Source/AppArguments/RunOptions.cs
@@ -1,27 +1,28 @@
 ﻿using CommandLine;
 
-namespace ProjectBuilder.AppArguments;
-
-[Verb("process", HelpText = "Run app with arguments")]
-public class RunOptions
+namespace ProjectBuilder.AppArguments
 {
-    [Option('p', "path", Required = true,
-        HelpText = "Full path to container solution"
-    )]
-    public string Path { get; set; }
+    [Verb("process", HelpText = "Run app with arguments")]
+    public class RunOptions
+    {
+        [Option('p', "path", Required = true,
+            HelpText = "Full path to container solution"
+        )]
+        public string Path { get; set; }
 
-    [Option('n', "name", Required = true,
-        HelpText = "Container name"
-    )]
-    public string Name { get; set; }
+        [Option('n', "name", Required = true,
+            HelpText = "Container name"
+        )]
+        public string Name { get; set; }
 
-    [Option('v', "vers", Required = true,
-        HelpText = "Container version"
-    )]
-    public string Version { get; set; }
+        [Option('v', "vers", Required = true,
+            HelpText = "Container version"
+        )]
+        public string Version { get; set; }
 
-    [Option('t', "token", Required = true,
-        HelpText = "Token"
-    )]
-    public string Token { get; set; }
+        [Option('t', "token", Required = true,
+            HelpText = "Token"
+        )]
+        public string Token { get; set; }
+    }
 }
